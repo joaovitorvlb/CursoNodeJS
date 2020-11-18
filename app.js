5c6b7c66fadcef7e56b5f589b91184de639cae7e
Joao Vitor Lansoni Barbosa <joaovitorvlb@hotmail.com>
2020-11-17 23:22:14 -0300

    Configurando rotas

--- app.js
+++ app.js
@@ -3,14 +3,18 @@ var app = express();
 
 app.set('view engine', 'ejs');
 
-app.get('/', function (req, res) {
-    res.send("<html><body>Portal de Notícias</body></html>");
+app.get('/', function(req,res){
+    res.render('home/index');
 });
 
-app.get('/tecnologia', function (req, res) {
-    res.render('secao/tecnologia');
+app.get('/formulario_inclusao_noticia', function(req,res){
+    res.render('admin/form_add_noticia');
 });
 
-app.listen(3000, function () {
+app.get('/noticias', function(req,res){
+    res.render('noticias/noticias');
+});
+
+app.listen(3000, function(){
     console.log("Servidor rodando com Express");
 });
