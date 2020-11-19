e3f579f433b62536a244fde9af69d8e9ed6f30d3
Joao Vitor Lansoni Barbosa <joaovitorvlb@hotmail.com>
2020-11-19 13:10:15 -0300

    Add recurso de autoload

--- config/dbConnection.js
+++ config/dbConnection.js
@@ -1,10 +1,16 @@
 var mysql = require('mysql');
 
-module.exports = function(){
+var connMySQL = function () {
+    console.log('Conexao com o bd foi estabelecida');
     return connection = mysql.createConnection({
         host: 'localhost',
         user: 'root',
         password: '',
         database: 'portal_noticias'
     });
+}
+
+module.exports = function () {
+    console.log('O autoload carregou o módulo de conexão com o bd');
+    return connMySQL;
 }
