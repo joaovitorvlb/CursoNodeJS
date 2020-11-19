25e85669e07006b2ed2a8be8da567a19689d5750
Joao Vitor Lansoni Barbosa <joaovitorvlb@hotmail.com>
2020-11-18 23:29:59 -0300

    Adicionado o require do modulo criiado

--- app/routes/noticias.js
+++ app/routes/noticias.js
@@ -1,18 +1,9 @@
-module.exports = function(app){
-    app.get('/noticias', function(req,res){
-        var mysql = require('mysql');
-
-        var connection = mysql.createConnection({
-            host: 'localhost',
-            user: 'root',
-            password: '',
-            database: 'portal_noticias'
+var dbConnection = require('../../config/dbConnection');
+module.exports = function (app) {
+    app.get('/noticias', function (req, res) {
+        var connection = dbConnection();
+        connection.query("SELECT * FROM noticias", function (error, result) {
+            res.render('noticias/noticias', { noticias: result });
         });
-
-        connection.query("SELECT * FROM noticias", function(error, result){
-            res.send(result);
-        });
-
-        //res.render('noticias/noticias');
     });
 }
