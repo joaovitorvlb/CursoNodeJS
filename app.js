72dd08e5395a7cc5bd33ca92a6ec3ad2baab215a
Joao Vitor Lansoni Barbosa <joaovitorvlb@hotmail.com>
2020-11-18 23:11:16 -0300

    Separando por modulos

--- app.js
+++ app.js
@@ -1,20 +1,11 @@
-var express = require('express');
-var app = express();
+var app = require('./config/server');
 
-app.set('view engine', 'ejs');
+var rotaNoticias = require('./app/routes/noticias')(app);
 
-app.get('/', function(req,res){
-    res.render('home/index');
-});
+var rotaHome = require('./app/routes/home')(app);
 
-app.get('/formulario_inclusao_noticia', function(req,res){
-    res.render('admin/form_add_noticia');
-});
+var rotaFormInclusaoNoticia = require('./app/routes/form_inclusao_noticia')(app);
 
-app.get('/noticias', function(req,res){
-    res.render('noticias/noticias');
-});
-
-app.listen(3000, function(){
-    console.log("Servidor rodando com Express");
+app.listen(3000, function () {
+    console.log("Servidor ON");
 });
